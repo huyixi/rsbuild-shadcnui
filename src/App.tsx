12670b38369f58b75beafc839f337f5749d0285a
huyixi <hi@huyixi.com>
2024-10-08 12:58:53 +0800

    feat: integrate tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import './App.css';
+import "./App.css";
 
 const App = () => {
   return (
     <div className="content">
+      <h1 className="text-red-500 text-3xl font-bold underline">Hello world!</h1>
       <h1>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
     </div>
